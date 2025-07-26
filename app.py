8af6002aeefc98083a6ffabd500abbac44b1844f
Jeewon Koo <jeewon514@gmail.com>
2025-07-27 01:19:50 +0900

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 # 홈화면
 @app.route('/')
 def defaultpage():
-    return render_template("index.html")
+    return render_template("fieldset.html")
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5500)
